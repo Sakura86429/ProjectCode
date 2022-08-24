a0f18085374b22e5985f06b52925a3ed283fb8ca
zhouh <441237909@qq.com>
2022-08-24 15:35:14 +0800

    ListNode

---
+++ src/career/ListNode.java
@@ -0,0 +1,22 @@
+package career;
+
+/**
+ * Topic
+ * Description
+ *
+ * @author zhouh
+ * @version 1.0
+ * Create by 2022/8/24 15:33
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) {
+        this.val = val;
+    }
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
