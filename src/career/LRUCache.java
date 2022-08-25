4e23d8f50fd134b1ec1c1f326ecd1514d9c5a76d
zhouh <441237909@qq.com>
2022-08-25 10:38:53 +0800

    LRUCache

---
+++ src/career/LRUCache.java
@@ -0,0 +1,87 @@
+package career;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Topic
+ * Description
+ *
+ * @author zhouh
+ * @version 1.0
+ * Create by 2022/8/25 10:28
+ */
+public class LRUCache {
+    class DLinkedNode {
+        int key;
+        int value;
+        DLinkedNode prev;
+        DLinkedNode next;
+        public DLinkedNode() {}
+        public DLinkedNode(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+    private Map<Integer, DLinkedNode> cache = new HashMap<>();
+    private int size;
+    private int capacity;
+    private DLinkedNode head, tail;
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        size = 0;
+        head = new DLinkedNode();
+        tail = new DLinkedNode();
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        DLinkedNode node = cache.get(key);
+        if (node == null) return -1;
+        else {
+            moveToHead(node);
+            return node.value;
+        }
+    }
+    public void put(int key, int value) {
+        DLinkedNode node = cache.get(key);
+        if (node == null) {
+            DLinkedNode newNode = new DLinkedNode(key, value);
+            cache.put(key, newNode);
+            addToHead(newNode);
+            size++;
+            if (size > capacity) {
+                DLinkedNode tail = removeTail();
+                cache.remove(tail.key);
+                size--;
+            }
+        } else {
+            node.value = value;   // 不更新cache中的value？？答：修改了，cache里面存的双向链表对象，node.value相当于修改了里面的链表值
+            moveToHead(node);
+        }
+    }
+
+    private DLinkedNode removeTail() {
+        DLinkedNode node = tail.prev;
+        removeNode(node);
+        return node;
+    }
+
+    private void moveToHead(DLinkedNode node) {
+        removeNode(node);
+        addToHead(node);
+    }
+
+    private void addToHead(DLinkedNode node) {
+        node.prev = head;
+        node.next = head.next;
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private void removeNode(DLinkedNode node) {
+        node.prev.next = node.next;   // 前一条线指向next
+        node.next.prev = node.prev;
+    }
+}
